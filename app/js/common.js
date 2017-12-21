5f6b7271e3a885c432d13530402ee5a029f9279e
Alex Klymenko <klymenko.sasha21@gmail.com>
2017-12-21 22:27:53 +0200

    add header on first screen, + logo

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
-	$(".main_head").css("min-height", $(window).height());
+	function heightDetect() {
+		$(".main_head").css("height", $(window).height());
+	};
+	heightDetect();
+	$(window).resize(function() {
+		heightDetect();
+	});
 	
+});
+
+$(window).load(function() {
+	
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+		
 });
